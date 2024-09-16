97d62cead4ad323a4698e3f3d6f18583a9c07726
luckys3000 <jwillard@396willard.com>
2024-09-16 09:44:36 -0500

    build: add callback code to validate password
     - Add additional callback code to validate any password a user enters

--- passport.js
+++ passport.js
@@ -1,7 +1,7 @@
-const passport = require("passport"),
-	LocalStrategy = require("passport-local").Strategy,
-	Models = require("./models.js"),
-	passportJWT = require("passport-jwt");
+const passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
+	Models = require('./models.js'),
+	passportJWT = require('passport-jwt');
 
 let Users = Models.User,
 	JWTStrategy = passportJWT.Strategy,
@@ -10,20 +10,24 @@ let Users = Models.User,
 passport.use(
 	new LocalStrategy(
 		{
-			usernameField: "Username",
-			passwordField: "Password",
+			usernameField: 'Username',
+			passwordField: 'Password',
 		},
 		async (username, password, callback) => {
 			console.log(`${username} ${password}`);
 			await Users.findOne({ Username: username })
 				.then((user) => {
 					if (!user) {
-						console.log("incorrect username");
+						console.log('incorrect username');
 						return callback(null, false, {
-							message: "Incorect username or password.",
+							message: 'Incorect username or password.',
 						});
 					}
-					console.log("finished");
+					if (!user.validatePassword(password)) {
+						console.log('incorrect password');
+						return callback(null, false, { message: 'Incorrect password.' });
+					}
+					console.log('finished');
 					return callback(null, user);
 				})
 				.catch((error) => {
@@ -40,7 +44,7 @@ passport.use(
 	new JWTStrategy(
 		{
 			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-			secretOrKey: "your_jwt_secret",
+			secretOrKey: 'your_jwt_secret',
 		},
 		async (jwtPayload, callback) => {
 			return await Users.findById(jwtPayload._id)

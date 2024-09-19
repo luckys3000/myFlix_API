b5c7b429cea9edcb55be2e691bc08c171f1a54e3
luckys3000 <jwillard@396willard.com>
2024-09-19 11:11:13 -0500

    fix: spelling mistake in model.js
     - Change "pasword" to "password"

--- models.js
+++ models.js
@@ -28,7 +28,7 @@ let userSchema = mongoose.Schema({
 });
 
 userSchema.statics.hashPassword = (password) => {
-	return bcrypt.hashSync(pasword, 10);
+	return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function (password) {

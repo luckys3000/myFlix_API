1498d8823fad98134f2c747bd15504e8df3238ee
luckys3000 <jwillard@396willard.com>
2024-09-30 15:10:17 -0500

    build: remove auth from movies enpoint
     - Remove passport.authenticate('jwt', { session: false }), from  /movies endpoint

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.CONNECTION_URI || 'mongodb://localhost:27017/cfDB',
  * @route GET /movies
  * @returns {object[]} List of movies
  */
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/movies', async (req, res) => {
 	await Movies.find()
 		.then((movies) => {
 			res.status(201).json(movies);
